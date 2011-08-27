da7dd2a9588f588cdb0c6835db3cbc00a9485448
jimmytidey <jimmytidey@gmail.com>
2011-08-27 10:18:51 +0100

    adding a bounding box, error handling for HTML geolocation

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 Scope = new Object(); 
 
 Scope.apiEndPoint = '/ephemerascope/api/index.php';
-
+Scope.pointsLat = new Array();
+Scope.pointsLng = new Array();
+Scope.wayPoints = new Array();
 
 Scope.scan = function() {
-	Scope.scanTweets();
 	Scope.scanApi();
 }
 
@@ -15,26 +16,68 @@ Scope.scanTweets = function() {
 
 //adds everything apart from Twitter, which I'm doing separately 
 Scope.scanApi = function() {
-	var url = Scope.apiEndPoint+'?lat='+Scope.lat+'&lng='+Scope.lng; 
+	
+	//remove any previous info 
+	Scope.map.removeAllMarkers();
+	Scope.map.removeAllPolylines()
+	Scope.wayPoints.length = 0;
+	Scope.pointsLat.length = 0;
+	Scope.pointsLng.length = 0;
+	
+	//put up a loader gif 
+	$('#loader').html('<img src="resources/ajax-loader.gif" alt="loader" />');
+	
+	//zoom in beacuse we can only search a small area 
+	Scope.map.setZoom(16);
+	
+	Scope.mapCenter = Scope.map.getCenter(); 
+	
+	var url = Scope.apiEndPoint+'?lat='+Scope.mapCenter.lat+'&lng='+Scope.mapCenter.lng; 
 	$.getJSON(url, function(data) {
+		
+		$('#loader').html('');
 		
 		Scope.apiData = eval(data);
 		
 		//foursquare tips 
 		$.each(Scope.apiData['fourSquareTips'], function(index, value) { 
-			Scope.addMarker(value.lat, value.lng, value['name']); 		
+			Scope.addMarker(value.lat, value.lng, value['name']); 	
+			Scope.pointsLat.push(value.lat);
+			Scope.pointsLng.push(value.lng);
 		});
 
 		//flickr 
 		$.each(Scope.apiData['flickr'], function(index, value) { 
-			Scope.addMarker(value.lat, value.lng, "<img src='"+value['url']+"' height='140' />"); 		
+			Scope.addMarker(value.lat, value.lng, "<img src='"+value['url']+"' height='140' />"); 	
+			Scope.pointsLat.push(value.lat);
+			Scope.pointsLng.push(value.lng);	
 		});
 		
+		Scope.wayPoints[0] = [Scope.pointsLat.min(), Scope.pointsLng.min()]; 
+		Scope.wayPoints[1] = [Scope.pointsLat.min(), Scope.pointsLng.max()]; 
+		Scope.wayPoints[2] = [Scope.pointsLat.max(), Scope.pointsLng.max()]; 
+		Scope.wayPoints[3] = [Scope.pointsLat.max(), Scope.pointsLng.min()]; 
+		Scope.wayPoints[4] = [Scope.pointsLat.min(), Scope.pointsLng.min()]; 
+		
+		var myPoly = new Polyline([
+		new LatLonPoint(Scope.wayPoints[0][0],Scope.wayPoints[0][1]),
+		new LatLonPoint(Scope.wayPoints[1][0],Scope.wayPoints[1][1]),
+		new LatLonPoint(Scope.wayPoints[2][0],Scope.wayPoints[2][1]),
+		new LatLonPoint(Scope.wayPoints[3][0],Scope.wayPoints[3][1]),
+		new LatLonPoint(Scope.wayPoints[4][0],Scope.wayPoints[4][1])
+		]);
+		
+		Scope.map.addPolyline(myPoly);
+	
 	});	
+	
+	
+	
 } 
 
 
-Scope.drawMap = function(position) {	
+Scope.drawLocationMap = function(position) {	
+	
 	
 	//get the coords 
     Scope.lat =  position.coords.latitude;
@@ -44,26 +87,66 @@ Scope.drawMap = function(position) {
 
 	var myPoint = new LatLonPoint(Scope.lat, Scope.lng);
 
-	Scope.map.setCenterAndZoom(myPoint, 17);
+	Scope.map.setCenterAndZoom(myPoint, 15);
 
 	Scope.map.addControls({
 		pan: true, 
 		zoom: 'small',
 		map_type: true 
 	});	
-	
-	Scope.map.isLoaded(Scope.scan());
+
 }
 
+Scope.drawLondonMap = function() {	
+	
+	//get the coords 
+    Scope.lat =  '51.5001524';  
+    Scope.lng =  '-0.1262362';	
+
+	Scope.map = new Mapstraction('map_canvas', 'openstreetmap');
+
+	var myPoint = new LatLonPoint(Scope.lat, Scope.lng);
+
+	Scope.map.setCenterAndZoom(myPoint, 14);
+
+	Scope.map.addControls({
+		pan: true, 
+		zoom: 'small',
+		map_type: true 
+	});	
+
+}
 
 //initialise page 
 $('#home_page').live('pageshow', function() { 
-	//put the map on the page 
-
 	
-	//find out the location 	
-	navigator.geolocation.getCurrentPosition(Scope.drawMap);
+	if (navigator.geolocation) {
+	
+	    navigator.geolocation.getCurrentPosition(
+	        function(position) {
+				alert('sucess');
+	        },
+	        function errorCallback(error) {
+				alert("Your HTML 5 geolocation isn't woking");
+				Scope.drawLondonMap(); 
+	        },
+	        {
+	            maximumAge:Infinity,
+	            timeout:1000
+	        }
+	    );
 
+	} 
+
+	else {
+		alert("Your HTML 5 geolocation isn't woking");
+		Scope.drawLondonMap();
+	}	
+
+
+	$('#wine_glass').click(function() {
+		Scope.scan();
+	});
 });		
 
 
@@ -73,14 +156,29 @@ Scope.addMarker = function advancedMarker(lat, lng, info) {
         label : info,
         date : "new Date()",
         marker : 4,
-        iconShadow: "http://mapufacture.com/images/providers/blank.png",
+        iconShadow: "resources/marker_shadow.png",
         iconShadowSize : [0,0],
-        icon : "http://assets1.mapufacture.com/images/markers/usgs_marker.png",
+        icon : "resources/blue_marker.png",
         iconSize : [20,20],
         draggable : false,
         hover : true
     });
     
+}
+
+
+Array.prototype.max = function() {
+	var max = this[0];
+	var len = this.length;
+	for (var i = 1; i < len; i++) if (this[i] > max) max = this[i];
+	return max;
+}
+
+Array.prototype.min = function() {
+	var min = this[0];
+	var len = this.length;
+	for (var i = 1; i < len; i++) if (this[i] < min) min = this[i];
+	return min;
 }
 
  
